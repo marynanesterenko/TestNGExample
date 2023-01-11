a36e47f503e19067409a9d1fbfd79731937ad973
marynanesterenko <marynanesterenko95@gmail.com>
2023-01-10 21:59:43 -0500

    Pre-requisites added to the BaseClass

--- src/test/java/com/automation/test/BaseTest.java
+++ src/test/java/com/automation/test/BaseTest.java
@@ -3,7 +3,7 @@ package com.automation.test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseTest {
+public abstract class BaseTest {
     @BeforeMethod
     public void setUp(){
         System.out.println("Before any test method");
